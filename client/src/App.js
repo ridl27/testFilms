8a359b3f31a9b1edfe61ddeccc6d515ec94cf723
Alex Kovry <okovryzhenko27@ukr.net>
2019-03-26 13:49:15 +0200

    test

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,19 @@ import Film from './Components/Film';
 import Form from './Components/Form';
 
 class App extends Component {
-
 	render() {
 	    return (
-				<BrowserRouter>
-					<div className="App">
-						<Switch>
-							<Route exact path='/' component={Home}/>
-							<Route path='/add_film' component={Form} />
-							<Route path='/:film_id' component={Film} />
-						</Switch>
-					</div>
-				</BrowserRouter>
+			<BrowserRouter>
+				<div className="App">
+					<Switch>
+						<Route exact path='/' component={Home}/>
+						<Route path='/add_film' component={Form} />
+						<Route path='/:film_id' component={Film} />
+					</Switch>
+				</div>
+			</BrowserRouter>
 	    );
-  }
+    }
 }
 
 export default App;
